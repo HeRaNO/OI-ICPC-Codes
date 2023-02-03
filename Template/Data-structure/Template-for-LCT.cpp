a36443ef9941c91019f7aad11ea9e044782319b1
HeRaNO <heran55@126.com>
2023-02-04 00:33:10 +0800

    update LCT

--- Template/Data-structure/Template-for-LCT.cpp
+++ Template/Data-structure/Template-for-LCT.cpp
@@ -1,161 +1,157 @@
 #include <cstdio>
 #include <algorithm>
-#define MAXN 100010
+#define MAXN 200010
 using namespace std;
 
-struct SplayTree
+struct LCT
 {
-	int ch[2], father;
-	int sum;
-	bool rev;
+	int ch[MAXN][2], fa[MAXN];
+	long long path_s[MAXN], sub_s[MAXN], w[MAXN], v[MAXN];
+	bool rev[MAXN];
+
+	void pushUp(int x)
+	{
+		int l = ch[x][0], r = ch[x][1];
+		path_s[x] = path_s[l] + path_s[r] + w[x];
+		sub_s[x] = sub_s[l] + sub_s[r] + w[x] + v[x];
+		return ;
+	}
+
+	void filp(int x)
+	{
+		rev[x] = (!rev[x]);
+		swap(ch[x][0], ch[x][1]);
+		return ;
+	}
+
+	void pushDown(int x)
+	{
+		if (!rev[x]) return ;
+		if (ch[x][0]) filp(ch[x][0]);
+		if (ch[x][1]) filp(ch[x][1]);
+		rev[x] = false;
+		return ;
+	}
+
+	bool isRoot(int x)
+	{
+		return !(ch[fa[x]][0] == x || ch[fa[x]][1] == x);
+	}
+
+	bool isRightCh(int x)
+	{
+		return ch[fa[x]][1] == x;
+	}
+
+	void rotate(int x)
+	{
+		int y = fa[x], z = fa[y];
+		int chx = (ch[y][1] == x), chy = (ch[z][1] == y);
+		if (!isRoot(y)) ch[z][chy] = x;
+		ch[y][chx] = ch[x][chx ^ 1]; fa[ch[x][chx ^ 1]] = y;
+		ch[x][chx ^ 1] = y; fa[y] = x; fa[x] = z;
+		pushUp(y); pushUp(x);
+		return ;
+	}
+
+	void update(int x)
+	{
+		if (!isRoot(x)) update(fa[x]);
+		pushDown(x);
+		return ;
+	}
+
+	void splay(int x)
+	{
+		update(x);
+		for (int f = fa[x]; f = fa[x], !isRoot(x); rotate(x))
+			if (!isRoot(f))
+				rotate(isRightCh(x) == isRightCh(f) ? f : x);
+		return ;
+	}
+
+	void access(int x)
+	{
+		for (int i = 0; x; i = x, x = fa[x])
+		{
+			splay(x);
+			v[x] += sub_s[ch[x][1]] - sub_s[i];
+			ch[x][1] = i, pushUp(x);
+		}
+		return ;
+	}
+
+	void makeRoot(int x)
+	{
+		access(x); splay(x); filp(x);
+		return ;
+	}
+
+	void Init(int x, int y)
+	{
+		w[x] = y; pushUp(x);
+		return ;
+	}
+
+	void Link(int x, int y)
+	{
+		makeRoot(x); access(y); splay(y);
+		fa[x] = y; v[y] += sub_s[x]; pushUp(y);
+		return ;
+	}
+
+	void Cut(int x, int y)
+	{
+		makeRoot(x); access(y); splay(y);
+		ch[y][0] = fa[x] = 0; pushUp(y);
+		return ;
+	}
+
+	void Modify(int x, int y)
+	{
+		makeRoot(x); w[x] += y; pushUp(x);
+		return ;
+	}
+
+	long long QueryPath(int x, int y)
+	{
+		makeRoot(x); access(y); splay(y);
+		return path_s[y];
+	}
+
+	long long QuerySubTree(int x, int p)
+	{
+		makeRoot(p); access(x); splay(x);
+		return v[x] + w[x];
+	}
 };
 
-SplayTree tree[MAXN];
-int n, T, opt, u, v, w[MAXN];
-int sta[MAXN], top;
-
-inline void Push_Up(int x)
-{
-	int l = tree[x].ch[0], r = tree[x].ch[1];
-	tree[x].sum = tree[l].sum + tree[r].sum + w[x];
-	return ;
-}
-
-inline void Push_Down(int x)
-{
-	int l = tree[x].ch[0], r = tree[x].ch[1];
-	if (tree[x].rev)
-	{
-		tree[x].rev = false;
-		tree[l].rev ^= 1;
-		tree[r].rev ^= 1;
-		swap(tree[x].ch[0], tree[x].ch[1]);
-	}
-	return ;
-}
-
-inline bool isroot(int x)
-{
-	return !(tree[tree[x].father].ch[0] == x || tree[tree[x].father].ch[1] == x);
-}
-
-inline void Rotate(int &x)
-{
-	int y = tree[x].father, z = tree[y].father, l, r;
-	l = (tree[y].ch[1] == x);
-	r = l ^ 1;
-	if (!isroot(y)) tree[z].ch[tree[z].ch[1] == y] = x;
-	tree[x].father = z;
-	tree[y].father = x;
-	tree[tree[x].ch[r]].father = y;
-	tree[y].ch[l] = tree[x].ch[r];
-	tree[x].ch[r] = y;
-	Push_Up(y);
-	Push_Up(x);
-	return ;
-}
-
-inline void Splay(int x)
-{
-	top = 0;
-	sta[++top] = x;
-	for (int i = x; !isroot(i); i = tree[i].father) sta[++top] = tree[i].father;
-	while (top) Push_Down(sta[top--]);
-	while (!isroot(x))
-	{
-		int y = tree[x].father, z = tree[y].father;
-		if (!isroot(y))
-		{
-			if (tree[y].ch[0] == x ^ tree[z].ch[0] == y) Rotate(x);
-			else Rotate(y);
-		}
-		Rotate(x);
-	}
-	return ;
-}
-
-inline void Access(int x)
-{
-	for (int i = 0; x; i = x, x = tree[x].father) Splay(x), tree[x].ch[1] = i, Push_Up(x);
-	return ;
-}
-
-inline void Makeroot(int x)
-{
-	Access(x);
-	Splay(x);
-	tree[x].rev ^= 1;
-	return ;
-}
-
-inline void Link(int x, int y)
-{
-	Makeroot(x);
-	tree[x].father = y;
-	return ;
-}
-
-inline void Cut(int x, int y)
-{
-	Makeroot(x);
-	Access(y);
-	Splay(y);
-	tree[y].ch[0] = tree[x].father = 0;
-	return ;
-}
-
-inline int getroot(int x)
-{
-	Access(x);
-	Splay(x);
-	while (tree[x].ch[0]) x = tree[x].ch[0];
-	return x;
-}
-
-inline void read(int &x)
-{
-	x = 0;
-	int f = 1;
-	char ch = getchar();
-	while (ch < '0' || ch > '9')
-	{
-		if (ch == '-') f = -1;
-		ch = getchar();
-	}
-	while (ch >= '0' && ch <= '9') x = x * 10 + ch - '0', ch = getchar();
-	x *= f;
-	return ;
-}
+LCT T;
+int n, Q, opt, u, v;
 
 int main()
 {
-	read(n);
-	read(T);
-	for (int i = 1; i <= n; i++) read(w[i]), tree[i].sum = w[i];
-	while (T--)
+	scanf("%d %d", &n, &Q);
+	for (int i = 1; i <= n; i++) scanf("%d", &u), T.Init(i, u);
+	for (int i = 1; i < n; i++) scanf("%d %d", &u, &v), T.Link(u + 1, v + 1);
+	while (Q--)
 	{
-		read(opt);
-		read(u);
-		read(v);
-		if (!opt) Link(u, v); //Link a<->b
-		else if (opt == 1) Cut(u, v) //Cut a<->b
-			else if (opt == 3) //Change the weight of point u to v
-			{
-				Makeroot(u);
-				w[u] = v;
-				Push_Up(u);
-			}
-			else//Query the sum of weight on link u->v
-			{
-				if (getroot(u) != getroot(v)) puts("impossible");
-				else
-				{
-					Makeroot(u);
-					Access(v);
-					Splay(v);
-					printf("%d\n", tree[v].sum);
-				}
-			}
+		scanf("%d", &opt);
+		if (!opt)
+		{
+			scanf("%d %d", &u, &v); T.Cut(u + 1, v + 1);
+			scanf("%d %d", &u, &v); T.Link(u + 1, v + 1);
+		}
+		else if (opt == 1)
+		{
+			scanf("%d %d", &u, &v);
+			T.Modify(u + 1, v);
+		}
+		else
+		{
+			scanf("%d %d", &u, &v);
+			printf("%lld\n", T.QuerySubTree(u + 1, v + 1));
+		}
 	}
 	return 0;
 }
