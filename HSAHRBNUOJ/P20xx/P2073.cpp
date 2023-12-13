dacbfdf01aba046d406f68601cbd4cd11a81e0b6
HeRaNO <heran55@126.com>
2023-12-13 16:37:33 +0800

    fix P2073

--- HSAHRBNUOJ/P20xx/P2073.cpp
+++ HSAHRBNUOJ/P20xx/P2073.cpp
@@ -1,89 +1,52 @@
-#include <queue>
 #include <cstdio>
-#include <cstring>
-#define MAXN 100010
-using namespace std;
-
-struct link
-{
-	int to;
-	int val;
-	int nxt;
-};
-
-link e[MAXN * 2];
-int edge_num[MAXN], cnt;
-int n, m, u, v;
-int a[MAXN], dis[MAXN];
-bool vis[MAXN];
-queue <int> q;
-
-inline int gcd(int a, int b)
-{
-	return b ? gcd(b, a % b) : a;
-}
-inline int lcm(int a, int b)
-{
-	return a * b / gcd(a, b);
-}
-inline int getdis(int u, int v)
-{
-	return lcm(a[u], a[v]);
-}
-inline int mymax(int a, int b)
-{
-	return a > b ? a : b;
-}
-
-inline void add(int u, int v, int w)
-{
-	e[cnt] = (link)
-	{
-		v, w, edge_num[u]
-	};
-	edge_num[u] = cnt++;
-	e[cnt] = (link)
-	{
-		u, w, edge_num[v]
-	};
-	edge_num[v] = cnt++;
-}
-
-inline int SPFA()
-{
-	q.push(1);
-	dis[1] = 0;
-	vis[1] = true;
-	while (!q.empty())
-	{
-		int u = q.front();
-		q.pop();
-		vis[u] = false;
-		for (int i = edge_num[u]; ~i; i = e[i].nxt)
-			if (dis[e[i].to] > mymax(dis[u], e[i].val))
-			{
-				dis[e[i].to] = mymax(dis[u], e[i].val);
-				if (!vis[e[i].to])
-				{
-					vis[e[i].to] = true;
-					q.push(e[i].to);
-				}
-			}
-	}
-	return dis[n];
-}
+#include <vector>
+#include <numeric>
+#include <algorithm>
+#include <functional>
 
 int main()
 {
-	memset(edge_num, -1, sizeof edge_num);
-	memset(dis, 0x7f, sizeof dis);
-	scanf("%d %d", &n, &m);
-	for (int i = 1; i <= n; i++) scanf("%d", &a[i]);
+	int n, m; scanf("%d %d", &n, &m);
+	std::vector<int> a(n);
+	for (int &i : a) scanf("%d", &i);
+	std::vector<std::pair<int, std::pair<int, int>>> edges;
 	for (int i = 1; i <= m; i++)
 	{
-		scanf("%d %d", &u, &v);
-		add(u, v, getdis(u, v));
+		int u, v; scanf("%d %d", &u, &v); --u; --v;
+		edges.push_back({std::lcm(a[u], a[v]), {u, v}});
 	}
-	printf("%d\n", SPFA());
+	std::sort(edges.begin(), edges.end());
+	std::vector<int> f(n);
+	std::iota(f.begin(), f.end(), 0);
+	std::vector<std::vector<int>> g(n);
+	int edge_cnt = 0;
+	for (auto [w, edge] : edges)
+	{
+		int u, v; std::tie(u, v) = edge;
+		std::function<int(int)> getf = [&](int x)
+		{
+			return x == f[x] ? x : f[x] = getf(f[x]);
+		};
+		int fu = getf(u), fv = getf(v);
+		if (fu != fv)
+		{
+			f[fv] = fu; ++edge_cnt;
+			g[u].push_back(v); g[v].push_back(u);
+			if (edge_cnt == n - 1)
+				break;
+		}
+	}
+	std::function<void(int, int)> dfs = [&](int x, int fa)
+	{
+		f[x] = fa;
+		for (int v : g[x])
+			if (v != fa) dfs(v, x);
+		return ;
+	};
+	dfs(0, -1);
+	int ans = 0;
+	for (int x = n - 1; x; x = f[x])
+		ans = std::max(ans, std::lcm(a[x], a[f[x]]));
+	printf("%d\n", ans);
 	return 0;
 }
